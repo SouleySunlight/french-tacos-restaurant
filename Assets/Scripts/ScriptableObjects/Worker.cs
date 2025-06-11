65a3f0222c911fe904f27c035c9cd18013fceafc
Souleyman BEN HADJ BELGACEM <souleyman@MacBook-Pro-de-Souleyman.local>
2025-06-11 13:52:14 +0200

    feat: create a worker SO

---
+++ Assets/Scripts/ScriptableObjects/Worker.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Worker", menuName = "Scriptable Objects/Worker")]
+public class Worker : ScriptableObject
+{
+    public int level = 1;
+    public float secondsBetweenTasks;
+    public int pricePerDay;
+    public WorkersRole role = WorkersRole.GRILL;
+
+}
+
+public enum WorkersRole
+{
+    GRILL,
+    CHECKOUT,
+    HOTPLATE
+}
