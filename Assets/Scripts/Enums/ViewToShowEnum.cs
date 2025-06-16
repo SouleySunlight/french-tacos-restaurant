11d70d152fefaae712d8d8e842d1af47c7780d78
Souleyman BEN HADJ BELGACEM <souleyman@MacBook-Pro-de-Souleyman.local>
2025-06-16 14:28:39 +0200

    feat: create a sauce gruyere view

--- Assets/Scripts/Enums/ViewToShowEnum.cs
+++ Assets/Scripts/Enums/ViewToShowEnum.cs
@@ -9,5 +9,6 @@ public enum ViewToShowEnum
     SHOP,
     UPGRADE,
     WORKERS,
-    FRYER
+    FRYER,
+    SAUCE_GRUYERE
 }
---
+++ Assets/Scripts/Visuals/SauceGruyereVisual.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class SauceGruyereVisual : MonoBehaviour, IView
+{
+
+}
