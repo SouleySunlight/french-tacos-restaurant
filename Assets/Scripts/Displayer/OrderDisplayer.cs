0817a2d42d300f3e6042d6372acf8196919f25ab
Souleyman BEN HADJ BELGACEM <souleyman@MacBook-Pro-de-Souleyman.local>
2025-08-15 21:42:12 +0200

    update ingredients order

--- Assets/Scripts/Displayer/OrderDisplayer.cs
+++ Assets/Scripts/Displayer/OrderDisplayer.cs
@@ -7,6 +7,7 @@ public class OrderDisplayer : MonoBehaviour
 {
     public Order orderData;
     [SerializeField] private GameObject ingredientPrefab;
+    private List<GameObject> ingredientObjects = new();
 
     void Start()
     {
@@ -15,13 +16,81 @@ public class OrderDisplayer : MonoBehaviour
 
     void UpdateOrder()
     {
-        var index = 0;
-        foreach (var ingredient in orderData.expectedOrder)
+        InstantiateMeat();
+        InstantiateSauce();
+        InstantiateVegetable();
+        InstantiateFries();
+        InstantiateSauceGruyere();
+
+        UpdatePosition();
+    }
+
+    void InstantiateMeat()
+    {
+        var meats = orderData.expectedOrder.FindAll(x => x.category == IngredientCategoryEnum.MEAT);
+        foreach (var meat in meats)
         {
             var ingredientGO = Instantiate(ingredientPrefab, transform);
-            var rectTransform = ingredientGO.GetComponent<RectTransform>();
-            ingredientGO.GetComponentInChildren<Image>().sprite = ingredient.processedSprite;
+            ingredientGO.GetComponentInChildren<Image>().sprite = meat.processedSprite;
 
+            ingredientObjects.Add(ingredientGO);
+        }
+    }
+
+    void InstantiateSauce()
+    {
+        var sauces = orderData.expectedOrder.FindAll(x => x.category == IngredientCategoryEnum.SAUCE);
+        foreach (var sauce in sauces)
+        {
+            var ingredientGO = Instantiate(ingredientPrefab, transform);
+            ingredientGO.GetComponentInChildren<Image>().sprite = sauce.processedSprite;
+
+            ingredientObjects.Add(ingredientGO);
+        }
+    }
+
+    void InstantiateVegetable()
+    {
+        var vegetables = orderData.expectedOrder.FindAll(x => x.category == IngredientCategoryEnum.VEGETABLE);
+        foreach (var vegetable in vegetables)
+        {
+            var ingredientGO = Instantiate(ingredientPrefab, transform);
+            ingredientGO.GetComponentInChildren<Image>().sprite = vegetable.processedSprite;
+
+            ingredientObjects.Add(ingredientGO);
+        }
+    }
+
+    void InstantiateFries()
+    {
+        var fries = orderData.expectedOrder.FindAll(x => x.category == IngredientCategoryEnum.FRIES);
+        foreach (var fry in fries)
+        {
+            var ingredientGO = Instantiate(ingredientPrefab, transform);
+            ingredientGO.GetComponentInChildren<Image>().sprite = fry.processedSprite;
+
+            ingredientObjects.Add(ingredientGO);
+        }
+    }
+
+    void InstantiateSauceGruyere()
+    {
+        var sauceGruyeres = orderData.expectedOrder.FindAll(x => x.category == IngredientCategoryEnum.SAUCE_GRUYERE);
+        foreach (var sauceGruyere in sauceGruyeres)
+        {
+            var ingredientGO = Instantiate(ingredientPrefab, transform);
+            ingredientGO.GetComponentInChildren<Image>().sprite = sauceGruyere.processedSprite;
+
+            ingredientObjects.Add(ingredientGO);
+        }
+    }
+
+    void UpdatePosition()
+    {
+        var index = 0;
+        foreach (var ingredient in ingredientObjects)
+        {
+            var rectTransform = ingredient.GetComponent<RectTransform>();
             rectTransform.anchorMin = new Vector2(0, 1);
             rectTransform.anchorMax = new Vector2(0, 1);
             rectTransform.anchoredPosition = new Vector2(30 + (index % 4) * 60, -30 + (index / 4) * -60);
