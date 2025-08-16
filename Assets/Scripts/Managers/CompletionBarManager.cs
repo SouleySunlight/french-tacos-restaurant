755880a273509b1bfa0d36e70369f29af4453d4b
Souleyman BEN HADJ BELGACEM <souleyman@MacBook-Pro-de-Souleyman.local>
2025-08-16 14:07:59 +0200

    create a completionbarvisual

--- Assets/Scripts/Managers/CompletionBarManager.cs
+++ Assets/Scripts/Managers/CompletionBarManager.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class CompletionBarManager : MonoBehaviour
 {
+    private CompletionBarVisual completionBarVisual;
+
+    void Awake()
+    {
+        completionBarVisual = FindFirstObjectByType<CompletionBarVisual>(FindObjectsInactive.Include);
+        completionBarVisual.UpdateVisual(0, 100);
+    }
+
 
 }
---
+++ Assets/Scripts/Visuals/CompletionBarVisual.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompletionBarVisual : MonoBehaviour
+{
+    [SerializeField] private Image completionBarImage;
+    [SerializeField] private TMP_Text completionText;
+
+    public void UpdateVisual(int currentNumberOfTacos, int target)
+    {
+        completionBarImage.fillAmount = (float)currentNumberOfTacos / target;
+        completionText.text = $"{currentNumberOfTacos}/{target}";
+    }
+}
