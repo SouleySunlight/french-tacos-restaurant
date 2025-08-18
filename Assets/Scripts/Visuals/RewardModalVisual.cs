986ed7cb53575cb7f01f0b8fe8794921cce3ff8c
Souleyman BEN HADJ BELGACEM <souleyman@MacBook-Pro-de-Souleyman.local>
2025-08-18 16:44:27 +0200

    display other reward modals

--- Assets/Scripts/Visuals/RewardModalVisual.cs
+++ Assets/Scripts/Visuals/RewardModalVisual.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Components;
 using UnityEngine.UI;
@@ -8,6 +9,10 @@ public class RewardModalVisual : MonoBehaviour
     [SerializeField] private LocalizeStringEvent rewardTitle;
     [SerializeField] private LocalizeStringEvent rewardSubtitle;
     [SerializeField] private Image image;
+    [SerializeField] private GameObject quantityDisplay;
+    [SerializeField] private TMP_Text previousValue;
+    [SerializeField] private TMP_Text newValue;
+
 
     public void ShowRewardModal(Reward reward)
     {
@@ -15,13 +20,43 @@ public class RewardModalVisual : MonoBehaviour
         {
             ShowIngredientRewardModal(reward.ingredientToUnlock);
         }
+        if (reward.rewardType == RewardType.PRICE_INCREASE)
+        {
+            ShowTacosPriceRewardModal();
+        }
+        if (reward.rewardType == RewardType.MORE_ORDERS)
+        {
+            ShowOrderRewardModal();
+        }
         rewardModal.SetActive(true);
     }
 
     void ShowIngredientRewardModal(Ingredient ingredient)
     {
+        quantityDisplay.SetActive(false);
         rewardTitle.StringReference.TableEntryReference = "REWARD.INGREDIENT";
         rewardSubtitle.StringReference.TableEntryReference = "INGREDIENT_" + ingredient.id.ToUpper();
         image.sprite = ingredient.processedSprite;
+        image.gameObject.SetActive(true);
+    }
+
+    void ShowTacosPriceRewardModal()
+    {
+        rewardTitle.StringReference.TableEntryReference = "REWARD.TACOS";
+        rewardSubtitle.StringReference.TableEntryReference = "REWARD.TACOS_SUBTITLE";
+        image.gameObject.SetActive(false);
+        previousValue.text = (GameManager.Instance.OrdersManager.GetTacosPrice() - 1).ToString();
+        newValue.text = GameManager.Instance.OrdersManager.GetTacosPrice().ToString();
+        quantityDisplay.SetActive(true);
+    }
+
+    void ShowOrderRewardModal()
+    {
+        rewardTitle.StringReference.TableEntryReference = "REWARD.ORDER";
+        rewardSubtitle.StringReference.TableEntryReference = "REWARD.ORDER_SUBTITLE";
+        image.gameObject.SetActive(false);
+        previousValue.text = (GameManager.Instance.OrdersManager.GetMaxNumberOfOrders() - 1).ToString();
+        newValue.text = GameManager.Instance.OrdersManager.GetMaxNumberOfOrders().ToString();
+        quantityDisplay.SetActive(true);
     }
 }
