b2c525d92c991934ed3366277f7fe9e84ff35b7f
Souleyman BEN HADJ BELGACEM <souleyman@MacBook-Pro-de-Souleyman.local>
2025-08-26 07:16:04 +0200

    play trash sound

--- Assets/Scripts/Movement/TrashMovement.cs
+++ Assets/Scripts/Movement/TrashMovement.cs
@@ -4,12 +4,15 @@ using UnityEngine.EventSystems;
 public class TrashMovement : MonoBehaviour, IDropHandler
 {
 
+    [SerializeField] private AudioClip trashShound;
+
     public void OnDrop(PointerEventData pointerEventData)
     {
         GameObject dropped = pointerEventData.pointerDrag;
         if (!dropped.TryGetComponent<TacosDisplayer>(out var tacosDisplayer)) { return; }
         if (PlayzoneVisual.currentView == ViewToShowEnum.GRILL)
         {
+            GameManager.Instance.SoundManager.PlaySFX(trashShound);
             GameManager.Instance.GrillManager.DiscardTacos(tacosDisplayer.tacosData);
         }
     }
