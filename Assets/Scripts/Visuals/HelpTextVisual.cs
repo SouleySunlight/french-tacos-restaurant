c5ff8443bec2ec1683573c405718aac3cef7c73b
Souleyman BEN HADJ BELGACEM <souleyman@MacBook-Pro-de-Souleyman.local>
2025-08-29 21:59:57 +0200

    only show 1 message

--- Assets/Scripts/Visuals/HelpTextVisual.cs
+++ Assets/Scripts/Visuals/HelpTextVisual.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 public class HelpTextVisual : MonoBehaviour
 {
     [SerializeField] private GameObject helpTextDisplayer;
+    private GameObject currentMessage = null;
 
     public void ShowMessage(string messageKey, string parameter = null)
     {
         var createdText = Instantiate(helpTextDisplayer, this.transform);
         createdText.GetComponent<HelpTextDisplayer>().ShowMessage(messageKey, parameter);
+        DestroyExisitingMessage();
         PositionMessage(createdText);
+        currentMessage = createdText;
 
     }
 
@@ -20,5 +23,12 @@ public class HelpTextVisual : MonoBehaviour
         rect.anchorMin = new Vector2(0f, 0.75f);
         rect.anchorMax = new Vector2(1, 0.75f);
         rect.anchoredPosition = new Vector2(0, 0);
+    }
+
+    void DestroyExisitingMessage()
+    {
+        if (currentMessage == null) { return; }
+        Destroy(currentMessage);
+        currentMessage = null;
     }
 }
